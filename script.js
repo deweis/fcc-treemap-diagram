c37085cdf7eee99d9d2bd09a2993e80f04d8d700
Dennis <deweis@gmx.ch>
2019-01-07 11:54:35 +0100

    Import movie data

--- script.js
+++ script.js
@@ -9,3 +9,24 @@ const svg = d3
   // http://svg.tutorial.aptico.de/start3.php?knr=3&kname=Dokumentstruktur%20I&uknr=3.5&ukname=Das%20Attribut%20viewBox
   .attr('viewBox', `0 0 800 532`)
   .attr('preserveAspectRatio', 'xMidYMid meet');
+
+/**
+ * Async load the movies data
+ */
+d3.queue()
+  .defer(
+    d3.json,
+    'https://cdn.rawgit.com/freeCodeCamp/testable-projects-fcc/a80ce8f9/src/data/tree_map/movie-data.json'
+  )
+  .await(drawChart);
+
+/**
+ * Main function triggered when data load is ready
+ * Responsible to draw all data within the svg
+ */
+function drawChart(error, movie_sales) {
+  if (error) {
+    console.log(error);
+  }
+  console.log(movie_sales);
+}
